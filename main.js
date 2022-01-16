392dd5ecd0a786a8dbd154ea6ff424ab0cdc32b5
hi-pp <95181259+hi-pp@users.noreply.github.com>
2022-01-16 12:50:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+array_1 = ['pen','paper','book','bottle'];
+random_no = Math.floor((Math.random()*array_1.length)+1);
+element_of_array = array_1[random_no];
+
+timer_counter = 0;
+timer_check = "";
+draw_sketch = "";
+answer_holder = "";
+score = 0;
